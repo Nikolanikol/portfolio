3b57dac82f825d7b8fd8b1a6901a9ad8826a9039
Nikolai <krezi3927@gmail.com>
2023-10-08 21:10:37 +0900

    First

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const btnDarkMode = document.querySelector('.dark-mode-btn');
+
+if (localStorage.getItem('darkMode') === 'dark') {
+    btnDarkMode.classList.add('dark-mode-btn--active');
+    document.body.classList.add('dark');
+}
+// включение темного режима по кнопке
+btnDarkMode.onclick = function () {
+    btnDarkMode.classList.toggle("dark-mode-btn--active")
+    const isDark = document.body.classList.toggle("dark")
+    if (isDark) {
+        localStorage.setItem('darkMode','dark')
+    } else {
+        localStorage.setItem('darkMode', 'light')
+    }
+}
